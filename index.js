c63ad9381739d6c97d5c61487525be1b0aebf75d
barath sam <barathsam555.p@gmail.com>
2023-10-13 11:24:37 +0530

    third commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://chat-app-website.herokuapp.com",
+    origin: "https://main--chatapp-website.netlify.app",
     credentials: true,
   },
 });
